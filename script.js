55ab5cc3cfcf9794955d4c0733a4cd03eab04c4c
Vishal Pawar <vishalpawar425@gmail.com>
2023-12-28 23:08:54 +0530

    refactor: refactored the html code to make it DRY using JSON file

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const webLinks = document.querySelectorAll('.web-link');
+const categoriesContainer = document.querySelector('.category-container');
+const url = 'API/link.json';
 
-webLinks.forEach(weblink => {
-    const anchorTag = weblink.querySelector('a');
-    const imgTag =  weblink.querySelector('img');
+fetch(url)
+.then(response => response.json() )
+.then(data => {
+    data.categories.forEach(category => {
+        const categoryColumn = document.createElement('div');
+        categoryColumn.classList.add('column');
 
-    let link = anchorTag.href;
-    weblink.setAttribute('title', link)
-    link = link.replace('https://', '').replace('http://', '').replace('www.','');
-    const imgSrc = getFavIconImg(link);
-    imgTag.src = imgSrc;
-    imgTag.alt = link;
+        const categoryHeading = document.createElement('h2');
+        categoryHeading.textContent = category.name;
+        categoryColumn.appendChild(categoryHeading);
+
+        const linkContainer = document.createElement('div');
+        linkContainer.classList.add('link-container');
+        categoryColumn.appendChild(linkContainer);
+
+        category.links.forEach(link => {
+            const webLink = document.createElement('div');
+            webLink.classList.add('web-link');
+            webLink.setAttribute('title', link.name)
+
+            const anchorTag = document.createElement('a');
+            anchorTag.href = link.url;
+
+            const imgTag = document.createElement('img');
+            const domainOnlyURL = link.url.replace('https://', '').replace('http://', '').replace('www.','');
+            const imgLink = `https://icon.horse/icon/${domainOnlyURL}`;
+            imgTag.src = imgLink;
+            imgTag.alt = link.name;
+
+            anchorTag.appendChild(imgTag);
+            webLink.appendChild(anchorTag);
+            
+
+            linkContainer.appendChild(webLink);
+        })
+        categoriesContainer.appendChild(categoryColumn);
+    });
 })
-
-function getFavIconImg(url) {
-    return `https://icon.horse/icon/${url}`;
-    // return `https://www.google.com/s2/favicons?domain=${url}&sz=512`
-    // return `https://icons.duckduckgo.com/ip3/${url}.ico`
-}
